bf09488e04136e1fe6ebcb5c0a1fbc0d7d5f1478
YulisaTepo <tep23001@byui.edu>
2024-10-21 21:38:00 -0500

    Hamburger

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -157,13 +157,13 @@ document.querySelector("#currentyear").textContent = new Date().getFullYear();
 /* This display the last modification*/
 document.querySelector("#lastModified").textContent = document.lastModified;
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+const hamburgerElement = document.querySelector("#hamburgerButton");
+const navElement = document.querySelector(".nav-menu");
+
+hamburgerElement.addEventListener("click", () => {
+    navElement.classList.toggle("open");
+    hamburgerElement.classList.toggle("open");
 })
-
 
 
